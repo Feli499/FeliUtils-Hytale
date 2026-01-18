e3f6223600406e2f634b85227cb0906ff313df11
Feli <maik@ehemanns.de>
2026-01-18 20:42:50 +0100

    Added SaveDataListener

--- src/main/java/de/feli490/utils/hytale/FeliUtilsPlugin.java
+++ src/main/java/de/feli490/utils/hytale/FeliUtilsPlugin.java
@@ -1,11 +1,19 @@
 package de.feli490.utils.hytale;
 
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import de.feli490.utils.hytale.events.PlayerReadySavePlayerDataEventListener;
+import de.feli490.utils.hytale.playerdata.AbstractPlayerDataLoader;
+import de.feli490.utils.hytale.playerdata.PlayerDataProvider;
+import de.feli490.utils.hytale.playerdata.json.SingleFileJsonPlayerDataLoader;
+import java.io.IOException;
 import java.util.logging.Level;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 public class FeliUtilsPlugin extends JavaPlugin {
+
+    private AbstractPlayerDataLoader playerDataLoader;
 
     public FeliUtilsPlugin(@NonNullDecl JavaPluginInit init) {
         super(init);
@@ -13,16 +21,34 @@ public class FeliUtilsPlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
+
+        try {
+            playerDataLoader = new SingleFileJsonPlayerDataLoader(getLogger(), getDataDirectory());
+        } catch (IOException e) {
+            getLogger().at(Level.SEVERE)
+                       .withCause(e)
+                       .log("Failed to initialize player data loader");
+        }
+
         getLogger().at(Level.INFO).log("FeliUtilsPlugin is setup!");
     }
 
     @Override
     protected void start() {
+
+        PlayerReadySavePlayerDataEventListener playerReadySavePlayerDataEventListener = new PlayerReadySavePlayerDataEventListener(getLogger(),
+                                                                                                                                   playerDataLoader);
+        getEventRegistry().registerGlobal(PlayerReadyEvent.class, playerReadySavePlayerDataEventListener);
+
         getLogger().at(Level.INFO).log("FeliUtilsPlugin is started!");
     }
 
     @Override
     protected void shutdown() {
         getLogger().at(Level.INFO).log("FeliUtilsPlugin is shutdown!");
+    }
+
+    public PlayerDataProvider getPlayerDataProvider() {
+        return playerDataLoader;
     }
 }
---
+++ src/main/java/de/feli490/utils/hytale/events/PlayerReadySavePlayerDataEventListener.java
@@ -0,0 +1,38 @@
+package de.feli490.utils.hytale.events;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import de.feli490.utils.hytale.playerdata.PlayerDataSaver;
+import de.feli490.utils.hytale.utils.PlayerUtils;
+import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+
+public class PlayerReadySavePlayerDataEventListener implements Consumer<PlayerReadyEvent> {
+
+    private final HytaleLogger logger;
+    private final PlayerDataSaver playerDataSaver;
+
+    public PlayerReadySavePlayerDataEventListener(HytaleLogger logger, PlayerDataSaver playerDataSaver) {
+        this.logger = logger;
+        this.playerDataSaver = playerDataSaver;
+    }
+
+    @Override
+    public void accept(PlayerReadyEvent playerReadyEvent) {
+        Player player = playerReadyEvent.getPlayer();
+        PlayerUtils.getPlayerRef(player)
+                   .thenAccept(playerRef -> {
+                       try {
+                           playerDataSaver.savePlayerData(playerRef.getUuid(), player.getDisplayName());
+                       } catch (IOException e) {
+                           logger.at(Level.WARNING)
+                                 .withCause(e)
+                                 .log("Failed to save player data for player: " + player.getDisplayName() + " (" + playerRef.getUuid()
+                                              + ")");
+                       }
+                   });
+
+    }
+}
